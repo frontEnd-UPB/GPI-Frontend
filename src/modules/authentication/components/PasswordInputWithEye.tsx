c1feac7ea32250819d3bef3a1ed33e255bab06e6
valentina <valentinacalvod1@upb.edu>
2026-03-06 23:10:10 -0400

    eye image to icon

--- src/modules/authentication/components/PasswordInputWithEye.tsx
+++ src/modules/authentication/components/PasswordInputWithEye.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Input, type InputProps } from "../../../core/components";
 import { cn } from "../../../ui/utils";
-import eyeIcon from "../assets/auth_eyeIcon_image.png";
+import { Eye, EyeOff } from "lucide-react";
 
 type PasswordInputWithEyeProps = Omit<InputProps, "type">;
 
@@ -28,15 +28,23 @@ export const PasswordInputWithEye: React.FC<PasswordInputWithEyeProps> = ({
         disabled={disabled}
         aria-label={isVisible ? "Hide password" : "Show password"}
       >
-        <img
-          src={eyeIcon}
-          alt="Toggle password visibility"
-          className={cn(
-            "size-5 transition-opacity",
-            isVisible ? "opacity-100" : "opacity-40",
-            disabled ? "opacity-30" : ""
-          )}
-        />
+        {isVisible ? (
+          <EyeOff
+            className={cn(
+              "size-5 transition-opacity",
+              isVisible ? "opacity-100" : "opacity-40",
+              disabled ? "opacity-30" : ""
+            )}
+          />
+        ) : (
+          <Eye
+            className={cn(
+              "size-5 transition-opacity",
+              isVisible ? "opacity-100" : "opacity-40",
+              disabled ? "opacity-30" : ""
+            )}
+          />
+        )}
       </button>
     </div>
   );
