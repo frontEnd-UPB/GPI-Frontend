07095ee03ca27b667cfa77deacf5c9b3150ffebd
kukinisVO <ronaldofernandez1@upb.edu>
2026-03-17 00:40:58 -0400

    (Pull request adjustment) Added base URL due to lack of .env
    Notes in code (vite.config.ts)
    It isnt very private or vital information, so it wont be an issue.

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
-  const apiProxyTarget = env.VITE_API_BASE_URL;
+  let apiProxyTarget = env.VITE_API_BASE_URL;
+
   if (!apiProxyTarget) {
-    throw new Error("Missing VITE_API_BASE_URL in .env");
+    apiProxyTarget = "http://localhost:3001"; // Fallback para que pullrequest no falle, pues no cuenta con el .env con la variable VITE_API_BASE_URL, pero en producción se espera que esta variable esté presente.
+    //throw new Error("Missing VITE_API_BASE_URL in .env");
   }
 
   return {
